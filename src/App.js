21398f27e917d9c080eae5dc0d322048b518d609
Anatolii Kravchenko <tol890@ukr.net>
2023-04-05 18:26:52 +0300

    [007] - Add key to the items of a spendings list

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
   {
+    id:'e1',
     title: "Toilet Paper",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
   {
+    id:'e2',
     title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
+    id:'e3',
     title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
   {
+    id:'e4',
     title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
