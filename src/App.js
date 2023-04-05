8e1723cb02067a3e2c421ebfe43d3d49c98e225a
Anatolii Kravchenko <tol890@ukr.net>
2023-04-05 17:17:17 +0300

    [006] - Add dynamic rendering of new expense items, using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-    {
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpanseHandler = expense => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses( prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   };
 
   return (
